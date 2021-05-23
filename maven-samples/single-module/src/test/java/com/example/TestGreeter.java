c6a6edc51c8d5d9cefc7c46e66ea3720cb997ac7
Sachin <sacharora99@gmail.com>
2021-05-23 10:35:57 +0530

    Sample Commit Greet Mess

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -38,7 +38,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
   @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMess() {
     String someone = "World, Thanks you making me Funny";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
